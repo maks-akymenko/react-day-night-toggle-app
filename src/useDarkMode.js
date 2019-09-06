8dd5233e0c3947dac998380811c063a21e6336be
maximakymenko <maks.akymenko@gmail.com>
2019-09-06 20:55:02 +0200

    refactor dark mode and prefer-color-scheme

--- src/useDarkMode.js
+++ src/useDarkMode.js
@@ -4,24 +4,27 @@ export const useDarkMode = () => {
   const [theme, setTheme] = useState('light');
   const [componentMounted, setComponentMounted] = useState(false);
 
+  const setMode = mode => {
+    window.localStorage.setItem('theme', mode)
+    setTheme(mode)
+  };
+
   const toggleTheme = () => {
     if (theme === 'light') {
-      window.localStorage.setItem('theme', 'dark')
-      setTheme('dark')
+      setMode('dark')
     } else {
-      window.localStorage.setItem('theme', 'light')
-      setTheme('light')
+      setMode('light')
     }
   };
 
   useEffect(() => {
     const localTheme = window.localStorage.getItem('theme');
 
-    if (localTheme) {
-      setTheme(localTheme);
-    } else {
-      window.localStorage.setItem('theme', 'light');
-    }
+    window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && !localTheme ?
+      setMode('dark') :
+      localTheme ?
+        setTheme(localTheme) :
+        setMode('light');
 
     setComponentMounted(true);
   }, []);
